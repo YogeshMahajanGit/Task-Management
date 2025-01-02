ce5c7ec7a6f1482a8a455b8182c318b50e878a33
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-01-02 04:11:30 -0800

    fix

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,6 +5,8 @@ import TaskCard from "./TaskCard";
 export default function Home() {
   const [open, setOpen] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [isDode, setIsDone] = useState(false);
 
   useEffect(() => {
@@ -19,6 +21,20 @@ export default function Home() {
 
   function handleModal() {
     setOpen((prev) => !prev);
+    setTitle("");
+    setContent("");
+  }
+
+  function handleAddTask(title, content) {
+    axios
+      .post(`${import.meta.env.VITE_SERVER_URL}/task`, { title, content })
+      .then((res) => {
+        const newTask = res.data;
+        const updatedTasks = [...tasks, newTask];
+        setTasks(updatedTasks);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      })
+      .catch((err) => console.error("Error adding task:", err));
   }
 
   return (
@@ -52,6 +68,8 @@ export default function Home() {
           >
             <div className="py-2 px-2 flex items-center rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:outline-indigo-600 mb-4">
               <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 type="text"
                 placeholder="Enter task"
                 className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm"
@@ -60,6 +78,8 @@ export default function Home() {
             <div className="w-90 ">
               <div className="relative w-full min-w-[200px]">
                 <textarea
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
                   className=" h-full min-h-[100px] w-full rounded-[7px] border border-blue-indigo-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all focus:border-2 focus:border-indigo-600 focus:border-t-transparent focus:outline-0"
                   placeholder="Message"
                 ></textarea>
@@ -68,6 +88,10 @@ export default function Home() {
             <button
               type="button"
               className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              onClick={() => {
+                handleAddTask(title, content);
+                handleModal();
+              }}
             >
               Done
             </button>
