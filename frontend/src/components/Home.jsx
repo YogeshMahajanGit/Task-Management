cfe27698de11bc3cf04e3c4c3387c4e172be8656
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-01-02 02:50:21 -0800

    add  task modal

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -29,13 +29,52 @@ export default function Home() {
           isDone={true}
         />
       </div>
-
       <div
-        className="w-12 h-12 text-4xl flex items-center justify-center font-semibold  bg-blue-400 rounded-full right-4 absolute cursor-pointer"
+        className="fixed bottom-16 right-6 w-14 h-14 flex items-center justify-center text-3xl font-bold bg-blue-400 text-white rounded-full shadow-lg cursor-pointer hover:bg-blue-500"
         onClick={handleModal}
       >
         +
       </div>
+
+      {open && (
+        <div
+          className="fixed inset-0 flex items-center justify-center z-50 bg-black/50"
+          onClick={() => setOpen(false)}
+        >
+          <div
+            className="relative w-[90%] flex flex-col gap-2 max-w-md border rounded p-10 bg-white shadow-md transition-transform duration-300 ease-in-out"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="py-2 px-2 flex items-center rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:outline-indigo-600 mb-4">
+              <input
+                type="text"
+                placeholder="Enter task"
+                className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm"
+              />
+            </div>
+            <div className="w-90 ">
+              <div className="relative w-full min-w-[200px]">
+                <textarea
+                  className=" h-full min-h-[100px] w-full rounded-[7px] border border-blue-indigo-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all focus:border-2 focus:border-indigo-600 focus:border-t-transparent focus:outline-0"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              Done
+            </button>
+            <button
+              className=" py-2 px-2 absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+              onClick={() => setOpen(false)}
+            >
+              ✖
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,4 +1,6 @@
 import Icon from "./Icon";
+import Del from "../assets/delete.svg";
+import Edit from "../assets/edit.svg";
 
 export default function TaskCard({
   title,
@@ -9,25 +11,24 @@ export default function TaskCard({
   onDelete,
 }) {
   return (
-    <div className=" w-80 border rounded p-4 bg-white hover:shadow-md transition-all ease-in-out">
-      <div className="flex items-center justify-between">
+    <div className=" flex gap-4 w-80 border rounded p-6 bg-white hover:shadow-md transition-all ease-in-out">
+      <div className=" flex flex-col  gap-6 ">
         <h4 className="text-2xl font-medium">{title}</h4>
+        <p className="">{content?.slice(0, 50)}</p>
         <span className="text-xs text-slate-500">{date}</span>
       </div>
-      <div>
-        <Icon />
+      <div className="flex flex-col-reverse gap-8 p-2 ">
+        <Icon svg={Del} />
+
+        <div className="">
+          <input
+            className="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 cursor-pointer"
+            type="checkbox"
+            checked={isDone}
+          />
+        </div>
+        <Icon svg={Edit} />
       </div>
-      <div className="flex items-center mb-4">
-        <input
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          type="checkbox"
-          checked={isDone}
-        />
-        <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-500">
-          Check do sume checkbox
-        </label>
-      </div>
-      <p className="">{content?.slice(0, 50)}</p>
     </div>
   );
 }
